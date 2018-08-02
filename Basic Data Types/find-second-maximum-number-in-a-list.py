71ac26897d98a690566979a700c2918cb5a9cf48
Rachit Sharma(rachit.sharma) <rachit.sharma@infosys.com>
2018-08-02 13:48:00 +0530

    Basic Data Types

--- Basic Data Types/find-second-maximum-number-in-a-list.py
+++ Basic Data Types/find-second-maximum-number-in-a-list.py
@@ -1,4 +1,4 @@
 #n = int(input())
-arr = [2,3,5,5]
+arr = [2,3,4,5]
 m = max(arr)
 print(max(list(filter(lambda a:a != max(arr), arr))))
---
+++ Basic Data Types/finding-the-percentage.py
@@ -0,0 +1,4 @@
+n = 3
+student_marks = {"Krishna":[67, 68, 69], "Arjun":[70, 98, 63], "Malika":[52, 56, 60]}
+query_name = "Malika"
+print("{:.2f}".format(float(sum(student_marks[query_name]))/3))
